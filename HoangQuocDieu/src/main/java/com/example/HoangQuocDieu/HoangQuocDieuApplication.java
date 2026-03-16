a4326360cfae95938da77414b16b1505de55d65a
Hoang Quoc Dieu <hoangquocdieu15@gmail.com>
2026-03-16 14:40:01 +0700

    BaiTapGitAcctions

---
+++ HoangQuocDieu/src/main/java/com/example/HoangQuocDieu/HoangQuocDieuApplication.java
@@ -0,0 +1,13 @@
+package com.example.HoangQuocDieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangQuocDieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoangQuocDieuApplication.class, args);
+	}
+
+}
---
+++ HoangQuocDieu/src/test/java/com/example/HoangQuocDieu/HoangQuocDieuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HoangQuocDieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangQuocDieuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
